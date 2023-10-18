f3abb0be7a998159093b44d3241d5fd49322e60d
Zico-C <Zico-C@users.noreply.github.com>
2023-10-18 09:41:33 +0000

    deploy: 6f50b52f2ed1c83b6ffbf1770ace3db3338b2da4

---
+++ assets/Page7_02-32052c53.js
@@ -0,0 +1,1 @@
+import{r as o,M as r,j as t}from"./index-1db4e143.js";function i(){const{count:n,add:s,minus:e}=o.useContext(r);return t.jsxs("div",{className:"Home",children:[t.jsx("h1",{children:"useContext 測試："}),t.jsxs("p",{children:["Count: ",n]}),t.jsx("button",{onClick:s,children:"增加"}),t.jsx("button",{onClick:e,children:"減少"})]})}export{i as default};
