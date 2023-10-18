bb7a9eccbf2dfa5a5fa3888281463df982011cb9
Zico-c <go29810756@gmail.com>
2023-10-18 10:07:14 +0800

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"Zico-C",
+  base:"/Zico-C/",
   resolve:{
     alias:{
       "@":path.resolve(__dirname,'./src')
